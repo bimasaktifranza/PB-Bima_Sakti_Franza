a829b5ebb495109e8fceb536bbfa5a54638d9c20
bimasaktifranza <12350111622@students.uin-suska.ac.id>
2025-03-22 12:39:36 +0800

    Commit

--- app/src/main/java/com/example/pemrogramanbergerak1/MainActivity3.java
+++ app/src/main/java/com/example/pemrogramanbergerak1/MainActivity3.java
@@ -1,24 +1,37 @@
 package com.example.pemrogramanbergerak1;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.cardview.widget.CardView;
 
 public class MainActivity3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main3);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        CardView cardProfile = findViewById(R.id.cardProfile);
+
+        cardProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity3.this, profileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btnProfile = findViewById(R.id.btn_profile);
+
+        btnProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity3.this, profileActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
